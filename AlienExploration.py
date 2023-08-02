af13f0f52b8e63a3fb446f524b12c1a232710cab
Bhdeme <71788650+BhadraDeviMenon@users.noreply.github.com>
2023-08-02 19:24:48 -0400

    Create AlienExploration.py

---
+++ AlienExploration.py
@@ -0,0 +1,1 @@
+print ("Hello World, my name is Gogy. I am a professional child alien trying to explore Earth. If anyone is reading this at all, please tell me why I'm getting chased my people in white lab coats?")
